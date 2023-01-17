d54c61a9f82365a7618ac10124930ca9c08a2264
Lynh <jenky.w0w@gmail.com>
2023-01-17 09:36:14 +0700

    Interceptor helper

---
+++ src/Middleware/Interceptor.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jenky\Atlas\Middleware;
+
+use Closure;
+use Jenky\Atlas\Request;
+use Jenky\Atlas\Response;
+
+class Interceptor
+{
+    /**
+     * Add an request interceptor.
+     */
+    public static function request(Closure $callback): Closure
+    {
+        return function (Request $request, Closure $next) use ($callback) : Response {
+            $callback($request);
+
+            return $next($request);
+        };
+    }
+
+    /**
+     * Add a response interceptor.
+     */
+    public static function response(Closure $callback): Closure
+    {
+        return function (Request $request, Closure $next) use ($callback) : Response {
+            $callback($response = $next($request));
+
+            return $response;
+        };
+    }
+}
--- tests/ConnectorTest.php
+++ tests/ConnectorTest.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Jenky\Atlas\Tests;
 
+use Closure;
+use Jenky\Atlas\Middleware\Interceptor;
+use Jenky\Atlas\Request;
+use Jenky\Atlas\Response;
 use Jenky\Atlas\Tests\Services\HTTPBin\Connector;
+use Jenky\Atlas\Tests\Services\HTTPBin\GetHeadersRequest;
 
 class ConnectorTest extends TestCase
 {
@@ -14,19 +19,20 @@ class ConnectorTest extends TestCase
 
         $this->assertCount(1, $connector->middleware());
 
-        $connector->middleware()->push(function () {
-            echo '123';
+        $connector->middleware()->push(function (Request $request, Closure $next): Response {
+            return $next($request);
         }, 'echo');
 
         $this->assertCount(2, $connector->middleware());
 
-        $connector->middleware()->before('echo', function () {
-        });
+        $connector->middleware()->before('echo', Interceptor::request(function () {
+        }));
 
-        $connector->middleware()->after('echo', function () {
-        });
+        $connector->middleware()->after('echo', Interceptor::response(function () {
+        }));
 
-        $connector->middleware()->prepend(function () {
+        $connector->middleware()->prepend(function (Request $request, Closure $next): Response {
+            return $next($request);
         }, 'first');
 
         $this->assertCount(5, $connector->middleware());
@@ -35,6 +41,18 @@ class ConnectorTest extends TestCase
 
         $this->assertSame('first', $middleware[0][1]);
         $this->assertSame('echo', $middleware[3][1]);
+
+        $connector->middleware()->push(function (Request $request, Closure $next): Response {
+            $request->headers()
+                ->with('X-Foo', 'bar');
+
+            return $next($request);
+        });
+
+        $response = $connector->send(new GetHeadersRequest);
+
+        $this->assertTrue($response->ok());
+        $this->assertSame('bar', $response->data('headers.X-Foo'));
     }
 
     public function test_requests_can_be_called_via_magic_method()
