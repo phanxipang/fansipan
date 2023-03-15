442d92d165b7661f6706cf41265d45e597be8bb0
Lynh <jenky.w0w@gmail.com>
2023-03-15 16:13:45 +0700

    Use docblock

--- src/Contracts/PipelineInterface.php
+++ src/Contracts/PipelineInterface.php
@@ -12,20 +12,24 @@ interface PipelineInterface
      * Set the traveler object being sent on the pipeline.
      *
      * @param  mixed  $traveler
+     * @return static
      */
-    public function send($traveler): PipelineInterface;
+    public function send($traveler);
 
     /**
      * Set the stops of the pipeline.
+     *
+     * @return static
      */
-    public function through(iterable $pipes): PipelineInterface;
+    public function through(iterable $pipes);
 
     /**
      * Push additional pipes onto the pipeline.
      *
      * @param  array|mixed  $pipes
+     * @return static
      */
-    public function pipe($pipes): PipelineInterface;
+    public function pipe($pipes);
 
     /**
      * Run the pipeline with a final destination callback.
