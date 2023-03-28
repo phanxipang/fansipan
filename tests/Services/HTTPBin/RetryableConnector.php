27665d8eca22e6379fa8b8ed9960b9d37e607588
Lynh <jenky.w0w@gmail.com>
2023-03-28 15:02:39 +0700

    Bug fixes

--- tests/Services/HTTPBin/RetryableConnector.php
+++ tests/Services/HTTPBin/RetryableConnector.php
@@ -4,20 +4,10 @@ declare(strict_types=1);
 
 namespace Jenky\Atlas\Tests\Services\HTTPBin;
 
-use GuzzleHttp\Client;
-use Jenky\Atlas\Connector;
 use Jenky\Atlas\Contracts\RetryableInterface;
 use Jenky\Atlas\Traits\Retryable;
-use Psr\Http\Client\ClientInterface;
 
-class RetryableConnector extends Connector implements RetryableInterface
+final class RetryableConnector extends Connector implements RetryableInterface
 {
     use Retryable;
-
-    protected function defineClient(): ClientInterface
-    {
-        return new Client([
-            'base_uri' => 'https://httpbin.org',
-        ]);
-    }
 }
