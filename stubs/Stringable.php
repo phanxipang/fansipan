ba03ade8f5707fa668c342845ecd9d452751e819
Lynh <jenky.w0w@gmail.com>
2023-05-05 23:50:53 +0700

    Revert return type

--- stubs/Stringable.php
+++ stubs/Stringable.php
@@ -8,6 +8,9 @@ declare(strict_types=1);
 if (\PHP_VERSION_ID < 80000) {
     interface Stringable
     {
-        public function __toString(): string;
+        /**
+         * @return string
+         */
+        public function __toString();
     }
 }
