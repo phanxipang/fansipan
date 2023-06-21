915e9d23a5b4cc421f14880df58b45949de4da2d
Lynh <jenky.w0w@gmail.com>
2023-06-21 09:25:36 +0700

    Auth (#13)
    * WIP on authenticator
    
    * Prepend Basic prefix
    
    * Add test
    
    * Add middleware name

---
+++ src/Middleware/Auth/TokenAuthentication.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jenky\Atlas\Middleware\Auth;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+final class TokenAuthentication
+{
+    /**
+     * @var string
+     */
+    private $token;
+
+    public function __construct(string $token, string $tokenPrefix = 'Bearer')
+    {
+        $this->token = $tokenPrefix.' '.trim($token);
+    }
+
+    public function __invoke(RequestInterface $request, callable $next): ResponseInterface
+    {
+        return $next($request->withHeader('Authorization', $this->token));
+    }
+}
---
+++ tests/AuthTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jenky\Atlas\Tests;
+
+use Jenky\Atlas\Middleware\Auth\BasicAuthentication;
+use Jenky\Atlas\Middleware\Auth\TokenAuthentication;
+use Jenky\Atlas\Mock\MockClient;
+use Jenky\Atlas\Mock\MockResponse;
+use Jenky\Atlas\NullConnector;
+use Jenky\Atlas\Tests\Services\DummyRequest;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+final class AuthTest extends TestCase
+{
+    private function fakeAuthentication(string $credential): callable
+    {
+        return static function (RequestInterface $request, callable $next) use ($credential): ResponseInterface {
+            if (! $request->hasHeader('Authorization')) {
+                return $next($request)->withStatus(401);
+            }
+
+            $authorization = $request->getHeaderLine('Authorization');
+
+            if ($authorization !== $credential) {
+                return $next($request)->withStatus(401);
+            }
+
+            return $next($request);
+        };
+    }
+
+    private function fakeBasicAuthentication(string $username, string $password): callable
+    {
+        $credential = base64_encode("$username:$password");
+
+        return $this->fakeAuthentication('Basic '.$credential);
+    }
+
+    private function fakeTokenAuthentication(string $token): callable
+    {
+        return $this->fakeAuthentication('Bearer '.$token);
+    }
+
+    public function test_basic_auth(): void
+    {
+        $client = new MockClient(
+            MockResponse::create(['authenticated' => true])
+        );
+        $connector = (new NullConnector())->withClient($client);
+        $connector->middleware()->push($this->fakeBasicAuthentication('foo', 'password'), 'fake_auth');
+
+        $response = $connector->send(new DummyRequest('http://localhost'));
+
+        $this->assertTrue($response->unauthorized());
+
+        $connector->middleware()->before('fake_auth', new BasicAuthentication('foo', 'password'));
+
+        $response = $connector->send(new DummyRequest('http://localhost'));
+
+        $this->assertTrue($response->ok());
+        $this->assertTrue($response['authenticated']);
+    }
+
+    public function test_token_auth(): void
+    {
+        $client = new MockClient();
+        $connector = (new NullConnector())->withClient($client);
+        $connector->middleware()->push($this->fakeTokenAuthentication('#zKh#4KNu$Bq4^b97KJ6'), 'fake_auth');
+
+        $response = $connector->send(new DummyRequest('http://localhost'));
+
+        $this->assertTrue($response->unauthorized());
+
+        $connector->middleware()->before('fake_auth', new TokenAuthentication('#zKh#4KNu$Bq4^b97KJ6'));
+
+        $response = $connector->send(new DummyRequest('http://localhost'));
+
+        $this->assertTrue($response->ok());
+    }
+}
