3a5d5ff20daad2cffa4fda0943ca4f54090ce299
Lynh <jenky.w0w@gmail.com>
2023-06-21 16:12:45 +0700

    Fix typo

--- src/Middleware/Interceptor.php
+++ src/Middleware/Interceptor.php
@@ -11,7 +11,7 @@ use Psr\Http\Message\ResponseInterface;
 final class Interceptor
 {
     /**
-     * Add an request interceptor.
+     * Add a request interceptor.
      */
     public static function request(Closure $callback): Closure
     {
