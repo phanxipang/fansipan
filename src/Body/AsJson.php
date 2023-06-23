049c4982a7b0878179693e1d81c4e996ad5cc682
Lynh <jenky.w0w@gmail.com>
2023-06-23 22:26:52 +0700

    Better way to create payload (#14)

--- src/Body/AsJson.php
+++ src/Body/AsJson.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 namespace Jenky\Atlas\Body;
 
+use Jenky\Atlas\Contracts\PayloadInterface;
+
 trait AsJson
 {
-    protected $bodyFormat = JsonPayload::class;
+    /**
+     * Create new JSON request body.
+     */
+    protected function definePayload(): PayloadInterface
+    {
+        return new JsonPayload(is_array($this->defaultBody()) ? $this->defaultBody() : []);
+    }
 }
