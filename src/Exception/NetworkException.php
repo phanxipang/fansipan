314d82d9d781a5948b73e18e97ab3e2fb66502dd
Lynh <jenky.w0w@gmail.com>
2023-07-13 11:25:33 +0700

    Add network exception

---
+++ src/Exception/NetworkException.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jenky\Atlas\Exception;
+
+use Psr\Http\Client\NetworkExceptionInterface;
+use Psr\Http\Message\RequestInterface;
+
+class NetworkException extends \RuntimeException implements NetworkExceptionInterface
+{
+    /**
+     * @var RequestInterface
+     */
+    private $request;
+
+    public function __construct(
+        string $message,
+        RequestInterface $request,
+        ?\Throwable $previous = null
+    ) {
+        parent::__construct($message, 0, $previous);
+        $this->request = $request;
+    }
+
+    public function getRequest(): RequestInterface
+    {
+        return $this->request;
+    }
+}
