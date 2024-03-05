cce61c5850642977366f4050201cebeee388e969
Lynh <jenky.w0w@gmail.com>
2024-03-05 22:10:15 +0700

    Implement ClientInterface in GenericConnector

--- src/GenericConnector.php
+++ src/GenericConnector.php
@@ -6,8 +6,9 @@ namespace Fansipan;
 
 use Fansipan\Contracts\ConnectorInterface;
 use Fansipan\Traits\ConnectorTrait;
+use Psr\Http\Client\ClientInterface;
 
-final class GenericConnector implements ConnectorInterface
+final class GenericConnector implements ConnectorInterface, ClientInterface
 {
     use ConnectorTrait;
 }
